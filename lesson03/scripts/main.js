0b3f081b0454004c6f2f58de0510c53de2157aec
Andrew Morris <wandusonmore@gmail.com>
2024-06-18 20:58:49 -0600

    s

--- lesson03/scripts/main.js
+++
@@ -1,8 +0,0 @@
-menubutton = document.querySelector("#menu-button");
-
-menubutton.addEventListener('click', () =>
-{
-    document.querySelector('#navbar').classList.toggle('active');
-    document.querySelector('#menu-open').classList.toggle('active');
-    document.querySelector('#menu-close').classList.toggle('active');
-});
