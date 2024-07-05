b04a40d50d9da841c9340001546581a2e7f148e2
Andrew Morris <wandusonmore@gmail.com>
2024-07-05 16:49:50 -0600

    s

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,14 +1,12 @@
 // select HTML elements in the document
-const currentTemp = document.querySelector('#temperature');
-const currentHumidity = document.querySelector('#humidity')
+const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=40.5566292&lon=-105.150053&appid=fbecddd063591d29cf733a7e9802066e&units=imperial'
+const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=36.1245484&lon=-115.5046478&appid=fbecddd063591d29cf733a7e9802066e&units=imperial'
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    currentHumidity.innerHTML = `${data.main.humidity}%`
     captionDesc.innerText = data.weather[0].main
     weatherIcon.setAttribute('src', " https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png")
     // const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
