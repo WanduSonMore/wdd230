c5e475c7f8c46208c6bb9a4b58464b4f69faff89
Andrew Morris <wandusonmore@gmail.com>
2024-07-23 18:21:17 -0600

    s

---
+++ final/scripts/forecast.js
@@ -0,0 +1,40 @@
+const WEATHER_URL2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=36.1245484&lon=-115.5046478&appid=fbecddd063591d29cf733a7e9802066e&units=imperial'
+
+
+function showCurrentTimeForcast(forecast){
+    const weatherElt = document.querySelector(".forecast")
+
+    const timenow = forecast[0].dt_txt.slice(11, 19)
+
+    let temps = forecast.filter(x => x.dt_txt.indexOf(timenow) != -1)
+
+    // Output 
+    for (let i=1; i<=3; i++){
+        let newsection = document.createElement("div")
+        let mydate = temps[i].dt_txt.slice(0, 10)
+        newsection.innerHTML = `
+        <img src="https://openweathermap.org/img/wn/${temps[i].weather[0].icon}@2x.png"}" alt="Weather Image">
+        <h2>${mydate}</h2>
+        <p>${temps[i].main.temp}&deg;F @ ${timenow}</p>`
+        weatherElt.append(newsection)
+
+    }
+}
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(WEATHER_URL2)
+      if (response.ok) {
+        const data = await response.json()
+        // console.log(data); // testing only
+        showCurrentTimeForcast(data.list) // uncomment when ready
+      } else {
+          throw Error(await response.text())
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+ 
+  apiFetch();
---
+++ final/scripts/weather.js
@@ -0,0 +1,32 @@
+
+  // select HTML elements in the document
+const currentTemp = document.querySelector('#temperature');
+const currentHumidity = document.querySelector('#humidity')
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#currentConditions');
+
+const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=40.5566292&lon=-105.150053&appid=fbecddd063591d29cf733a7e9802066e&units=imperial'
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    currentHumidity.innerHTML = `${data.main.humidity}%`
+    captionDesc.innerText = data.weather[0].main
+    weatherIcon.setAttribute('src', "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png")
+}
+
+async function apiFetch() {
+    try {
+      const response = await fetch(WEATHER_URL);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+ 
+  apiFetch();
